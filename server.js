96553fcf85a8bec606309c29256507209a7e724e
Luca <orksec@protonmail.com>
2021-05-22 20:51:53 +0200

    Peepeepoopoo check!

---
+++ server.js
@@ -0,0 +1,23 @@
+var port = 666;
+
+var express = require("express");
+const path = require("path");
+var app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static("source"));
+
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname, "source/index.html"));
+});
+
+server.listen(port, () => {
+	console.log("[SERVER] listening on *:3000");
+});
+
+io.on("connection", (socket) => {
+	console.log("[CLIENT] New client connection... (" + socket.id + ")");
+});

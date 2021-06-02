5f6deb3ecadcd48e189ea51470db4fb6eb7de03d
JesseB0rn <50794219+JesseB0rn@users.noreply.github.com>
2021-06-02 23:58:12 +0200

    implemented on device log window

--- source/js/index.js
+++ source/js/index.js
@@ -1,15 +1,21 @@
 currentFirmware = function(userAgent) {
     return userAgent.match(/\OS (.*?)\ like/)[1].replaceAll("_", ".");
 }
-
+function print(str) {
+            var log = document.getElementById('log');
+            console.log(str);
+            if (log) {
+                log.innerText += "[+] "+str + '\n';
+            }
+        }
 async function pwnMe() {
-	console.log(currentFirmware(navigator.userAgent));
+	print(currentFirmware(navigator.userAgent));
     if(currentFirmware(navigator.userAgent) === "14.5"){
-        console.log("Starting exploitation for iOS 14.5");
+        print("Starting exploitation for iOS 14.5");
     }else if(currentFirmware(navigator.userAgent) === "14.6"){
-        console.log("Starting exploitation for iOS 14.6");
+        print("Starting exploitation for iOS 14.6");
         await kickstart146();
     }else {
-        console.log("Detected a unsupported Device/Version")
+        print("Detected a unsupported Device/Version")
     }
 }

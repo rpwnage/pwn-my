26191c41fd496315ae3fe51efd5e1a3d5e2873e1
JesseB0rn <50794219+JesseB0rn@users.noreply.github.com>
2021-06-07 18:18:22 +0200

    added log window ui

--- source/js/ui.js
+++ source/js/ui.js
@@ -2,6 +2,10 @@ var stateReady   = document.getElementById('state-ready')
 var statePrgress = document.getElementById('state-inprogress')
 var stateDone    = document.getElementById('state-done')
 
+var preflightInstructions = document.getElementById('preflight-instructions')
+var logWindow = document.getElementById('log-window')
+var debriefInstructions = document.getElementById('debrief-instructions')
+
 function toggleRestoreFS() {
     console.log('toogled restore root fs')
 }
@@ -13,15 +17,24 @@ function transitionState(state) {
     if (state == 0) {
         console.log('transition main page')
         stateReady.style = ''
+        preflightInstructions.style = ''
     }
     if (state == 1) {
         console.log('transition in progress')
         statePrgress.style = ''
+        logWindow.style = ''
     }
 }
+function logUI(msg) {
+    logWindow.innerHTML += `[+] ${msg}<br>`
+}
+
 
 function setAllStatesInvisible() {
     stateReady.style = "display: none;"
     statePrgress.style = "display: none;"
     stateDone.style = "display: none;"
+    preflightInstructions.style = "display: none;"
+    logWindow.style = "display: none;"
+    //debriefInstructions.style  = "display: none;"
 }

5516b3d25bdd37e5f1f02666619ac4227e50d80c
Luca <orksec@protonmail.com>
2021-06-09 15:27:05 +0200

    fix bug (#10)

--- source/js/index.js
+++ source/js/index.js
@@ -17,6 +17,7 @@ async function pwnMe() {
 	document.getElementById("jbButton").disabled = true;
 	if (currentFirmware(navigator.userAgent) === "14.5") {
 		socket.emit("log_normal", "Starting exploitation for iOS 14.5");
+        await kickstart145();
 	} else if (currentFirmware(navigator.userAgent) === "14.6") {
 		socket.emit("log_normal", "Starting exploitation for iOS 14.6");
 		await kickstart146();
